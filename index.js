58d83ff5915a57cfa5e464a0fce010c5a141f6ef
Natalie Halloran <natalie.halloran@yahoo.com>
2018-04-23 00:25:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ const app = "I don't do much."
 function destructivelyAppendKitten(name) {
   kittens = [...kittens,name]
 } 
+function destructivelyPrependKitten(name) {
+  kittens = [name,...kittens]
+}
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
